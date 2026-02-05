813498d0a0e29151749413d0373bb68075bbde96
vladimirbutyrskii <vlaver@mail.ru>
2026-02-06 00:15:34 +0300

    add boolen-field 'new_item' to product model

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from django.forms import ModelForm
+from django.forms import ModelForm, BooleanField
 
 from catalog.models import Product
 
@@ -8,7 +8,10 @@ class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = "form-control"
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = "form-check-input"
+            else:
+                field.widget.attrs['class'] = "form-control"
 
 
 class ProductForm(StyleFormMixin, ModelForm):
